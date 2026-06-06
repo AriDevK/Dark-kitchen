ec347edb58caff12c21d0e07b9b14e31ab5771ef
AriDevK <AriadneFave@hotmail.com>
2026-06-05 20:02:20 -0600

    feat(database): added database configuration to config module, also created database module to wrap the database connection

--- packages/go/common/config/config.go
+++ packages/go/common/config/config.go
@@ -7,9 +7,14 @@ import (
 )
 
 type Config struct {
-	AppName string
-	AppEnv  string
-	AppPort string
+	AppName          string
+	AppEnv           string
+	AppPort          string
+	DatabaseHost     string
+	DatabasePort     string
+	DatabaseUser     string
+	DatabasePassword string
+	DatabaseName     string
 }
 
 func Load() Config {
@@ -24,8 +29,13 @@ func Load() Config {
 	_ = viper.ReadInConfig()
 
 	return Config{
-		AppName: viper.GetString("APP_NAME"),
-		AppEnv:  viper.GetString("APP_ENV"),
-		AppPort: viper.GetString("APP_PORT"),
+		AppName:          viper.GetString("APP_NAME"),
+		AppEnv:           viper.GetString("APP_ENV"),
+		AppPort:          viper.GetString("APP_PORT"),
+		DatabaseHost:     viper.GetString("DATABASE_HOST"),
+		DatabasePort:     viper.GetString("DATABASE_PORT"),
+		DatabaseUser:     viper.GetString("DATABASE_USER"),
+		DatabasePassword: viper.GetString("DATABASE_PASSWORD"),
+		DatabaseName:     viper.GetString("DATABASE_NAME"),
 	}
 }
---
+++ packages/go/common/database/database.go
@@ -0,0 +1,28 @@
+package database
+
+import (
+	"fmt"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func Connect(
+	host string,
+	port string,
+	user string,
+	password string,
+	dbName string,
+) (*gorm.DB, error) {
+
+	dsn := fmt.Sprintf(
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		host,
+		port,
+		user,
+		password,
+		dbName,
+	)
+
+	return gorm.Open(postgres.Open(dsn), &gorm.Config{})
+}
